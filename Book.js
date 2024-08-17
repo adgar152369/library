e8877a214fd6d01625204ea7ccd3111a2e339475
Adam <adgar1523@gmail.com>
2024-08-17 07:43:03 -0700

    allow read status

--- Book.js
+++ Book.js
@@ -16,7 +16,7 @@ function Book(title, author, pages, hasRead) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.hasRead = hasRead ? "Yes" : "No";
+  this.hasRead = hasRead ? true : false;
   this.isEditing = false;
   this.id = Math.random().toString(36).substr(2, 9)
 }
@@ -87,7 +87,7 @@ function displayBooks(library) {
 
     const hasReadCell = document.createElement("td");
     hasReadCell.setAttribute("scope", "col");
-    hasReadCell.textContent = bookData.hasRead;
+    hasReadCell.textContent = bookData.hasRead ? "Yes" : "No";
     bookRow.appendChild(hasReadCell);
 
     bookTable.appendChild(bookRow);
@@ -103,6 +103,7 @@ function openBookDialog(book, element, library) {
   const bookDialogHeader = document.querySelector('.book-dialog h3');
   const bookDialogClose = document.querySelector('.close-dialog');
   const bookDialogInputs = document.querySelectorAll('.book-dialog input:not([type="radio"])');
+  const bookDialogRadios = document.querySelectorAll('.book-dialog input[type="radio"]');
   const bookDialogBtns = document.querySelector('.action-btns');
   const bookDialogContainer = document.querySelector('#dialog-container');
 
@@ -129,7 +130,10 @@ function openBookDialog(book, element, library) {
     // set isEditing to true
     book.setIsEditing(true);
     
-    bookDialogBtns.innerHTML = "<button>Save</button><button class='deleteBookBtn'>Delete</button>";
+    bookDialogBtns.innerHTML = `
+      <button>Save</button>
+      <button class='deleteBookBtn'>Delete</button>
+    `;
     bookDialogHeader.textContent = "Edit book";
     bookDialog.showModal();
 
@@ -152,6 +156,9 @@ function openBookDialog(book, element, library) {
 
     // check if deleteBookBtn exists
     const deleteBookBtn = document.querySelector('.deleteBookBtn');
+    // const readStatusBtn = document.querySelector('.readStatusBtn');
+    
+    
     if (deleteBookBtn) {
       deleteBookBtn.addEventListener('click', (e) => {
         e.preventDefault();
@@ -162,7 +169,8 @@ function openBookDialog(book, element, library) {
 
     // TODO: save book
     // Close dialog without saving
-    bookDialogClose.addEventListener('click', () => {
+    bookDialogClose.addEventListener('click', (e) => {
+      e.preventDefault();
       book.setIsEditing(false);
       bookDialog.close();
     }, { once: true });
@@ -173,9 +181,12 @@ function openBookDialog(book, element, library) {
     bookDialogBtns.innerHTML = "<button type='submit'>Add Book</button>"
     bookDialog.showModal();    
 
-    form.addEventListener('submit', (e) => submitNewBookForm(e, library, bookDialogInputs, bookDialog), {once: true});
   }
   
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    submitNewBookForm(book,library, bookDialogInputs, bookDialog), {once: true}
+  });
   // set isEditing to false after modal closes:
   bookDialogClose.addEventListener('click', (e) => {
     e.preventDefault();
@@ -187,10 +198,37 @@ function deleteBook(book, library) {
   library.deleteBook(book);
   displayBooks(library);
 }
-function submitNewBookForm(e, library, bookInputs, bookDialog) {
-  e.preventDefault();
+
+function submitNewBookForm(book, library, bookInputs, bookDialog) {
+  console.log(book);
+
+
+  
   const bookDialogRadioInputs = document.querySelectorAll('.book-dialog input[type="radio"]');
   const radioChecked = Array.from(bookDialogRadioInputs).find((radio) => radio.checked);
+
+  if (book) {
+    const existingBookIndex = library.getBooks().findIndex(existingBook => existingBook ? existingBook.id === book.id : null);
+    if (existingBookIndex && existingBookIndex !== -1) {
+      const updatedBooks = library.getBooks().map(existingBook => {
+        if (existingBook.id === book.id) {
+          return {
+            ...existingBook,
+            title: bookInputs[0].value,
+            author: bookInputs[1].value,
+            pages: parseInt(bookInputs[2].value),
+            hasRead: radioChecked.value === "yes"
+          };
+        }
+        return existingBook;
+      });
+      library.books = updatedBooks;
+      displayBooks(library);
+      bookDialog.close();
+      return;
+    }
+  }
+
 
   // gather inputs and make new book when form submits
   const newBook = new Book (
@@ -205,5 +243,11 @@ function submitNewBookForm(e, library, bookInputs, bookDialog) {
 
 function handleAddBook(bookToAdd, library) {
   library.addBook(bookToAdd);
+  displayBooks(library);
+}
+
+function handleReadStatus(book, library) {
+  book.hasRead = !book.hasRead;
+  console.log(book)
   displayBooks(library);
 }
